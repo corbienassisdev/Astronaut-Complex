089d31f21d17abffa5f12aa43ce7fd111f0a08b0
Thibaud Laurent <thibaud.laurent6@gmail.com>
2016-12-16 16:47:20 +0100

    [Itération] Ajout du projet de tests unitaires. Courage, on arrive au bout !

---
+++ AstronautComplexTests/Test_ExerciceScore.cs
@@ -0,0 +1,39 @@
+﻿using AstronautComplex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AstronautComplexTests
+{
+    [TestClass]
+    public class Test_ExerciceScore
+    {
+        protected ExerciceScore score;
+
+        [TestMethod]
+        public void Test_NTotalAnswers_ToString()
+        {
+            score = new ExerciceScore();
+            score.GoodAnswers = 5;
+            score.TotalAnswers = 10;
+            Assert.AreEqual("50 %", score.ToString());
+        }
+
+        [TestMethod]
+        public void Test_0TotalAnswers_ToString()
+        {
+            score = new ExerciceScore();
+            score.GoodAnswers = 5;
+            score.TotalAnswers = 0;
+            Assert.AreEqual("∞ %", score.ToString());
+        }
+
+        [TestMethod]
+        public void Test_NotEnoughTotalAnswers_ToString()
+        {
+            score = new ExerciceScore();
+            score.GoodAnswers = 5;
+            score.TotalAnswers = 1;
+            Assert.AreEqual("500 %", score.ToString());
+        }
+    }
+}
